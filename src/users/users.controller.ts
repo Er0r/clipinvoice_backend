8cf75697232a696a3193e2898f979e949931cc99
Er0r <fahimmaria155@gmail.com>
2023-02-21 02:31:12 +0600

    Added Auth Guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Body, Get, Req, Post, ValidationPipe, UsePipes } from '@nestjs/common';
+import { Controller, Body, Get, Post, ValidationPipe, UsePipes, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './DTO/create-user.dto';
 import { LoginUserDto } from './DTO/login-user.dto';
 import { UserResponseInterface } from './types/userResponse.interface';
-import { Request } from 'express';
-import { ExpressRequest } from 'src/types/expressRequest.interface';
 import { UserDecorator } from './decorators/user.decorator';
 import { User } from './user.entity';
+import { AuthGuard } from './guards/auth.guard';
 
 @Controller()
 export class UsersController {
@@ -27,6 +26,7 @@ export class UsersController {
     }
 
     @Get('user')
+    @UseGuards(AuthGuard)
     async currentUser(@UserDecorator() user: User): Promise<UserResponseInterface> {
         return this.usersService.buildUserResponse(user);
     }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { AuthGuard } from './guards/auth.guard';
 
 @Module({
   imports: [ TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
+  providers: [UsersService, AuthGuard],
   controllers: [UsersController],
   exports: [UsersService]
 })
