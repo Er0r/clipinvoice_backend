b7bbb04bb1b753aee50efd41a15aee9c6e2eb8ff
Er0r <fahimmaria155@gmail.com>
2023-04-12 12:24:47 +0600

    Integrated Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.APP_PORT || 3000;
   app.setGlobalPrefix('api/v1');
+  const config = new DocumentBuilder()
+  .setTitle('Clipinvoice')
+  .setDescription('API Documentation')
+  .setVersion('1.0')
+  .addTag('clipinvoice')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);
   await app.listen(port);
 }
 bootstrap();
