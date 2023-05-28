74bee134e579725f5d844ddea365574e07b00268
Er0r <fahimmaria155@gmail.com>
2023-05-28 12:06:31 +0600

    Added cors with app.ts

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   const port = process.env.APP_PORT || 3000;
   app.setGlobalPrefix('api/v1');
   const config = new DocumentBuilder()
-  .setTitle('Clipinvoice')
-  .setDescription('API Documentation')
-  .setVersion('1.0')
-  .addTag('clipinvoice')
-  .build();
+    .setTitle('Clipinvoice')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .addTag('clipinvoice')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/v1', app, document);
   await app.listen(port);
